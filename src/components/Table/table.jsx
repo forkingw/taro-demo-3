1dde99efeebe32d1792ace592bb0f20e26ee08f0
for <eternallightwang@qq.com>
2022-08-14 17:01:01 +0800

    wip: 样式 使用他变了

--- src/components/Table/table.jsx
+++ src/components/Table/table.jsx
@@ -3,40 +3,52 @@ import { View } from "@tarojs/components";
 import './table.scss'
 
 export default class Table extends Component {
-  state = {
-    columns: [
-      {
-        title: '姓名',
-        dataIndex: 'name',
-      },
-      {
-        title: '年龄',
-        dataIndex: 'age'
-      }
-    ],
-    dataSource: [
-      {
-        name: '胡彦斌',
-        age: 32,
-        // address: '西湖区湖底公园1号',
-      },
-      {
-        name: '胡彦祖',
-        age: 42,
-        // address: '西湖区湖底公园1号',
-      }
-    ]
-  }
+  // state = {
+  //   columns: [
+  //     {
+  //       title: '姓名',
+  //       dataIndex: 'name',
+  //     },
+  //     {
+  //       title: '年龄',
+  //       dataIndex: 'age'
+  //     }
+  //   ],
+  //   dataSource: [
+  //     {
+  //       name: '胡彦斌',
+  //       age: 32,
+  //       // address: '西湖区湖底公园1号',
+  //     },
+  //     {
+  //       name: '胡彦祖',
+  //       age: 42,
+  //       // address: '西湖区湖底公园1号',
+  //     }
+  //   ]
+  // }
   render () {
-    const TableContent = this.state.columns.map(c => {
-      const cols = this.state.dataSource.map((d, i) => <View className='td' key={d.dataIndex + i}>{d[c.dataIndex]}</View>)
-      return <View className='col' key={c.dataIndex}>
-        <View className='th'>{c.title}</View>
-        {cols}
-      </View>
-    })
+    // const TableContent = this.state.columns.map(c => {
+    //   const cols = this.state.dataSource.map((d, i) => <View className='td' key={d.dataIndex + i}>{d[c.dataIndex]}</View>)
+    //   return <View className='col' key={c.dataIndex}>
+    //     <View className='th'>{c.title}</View>
+    //     {cols}
+    //   </View>
+    // })
     return <View className='table'>
-      {TableContent}
+      {/* {TableContent} */}
+      <View className='tbody'>
+        <View className='tr'>
+          <View className='th'>1</View>
+          <View className='th'>2</View>
+          <View className='th'>3</View>
+        </View>
+        <View className='tr'>
+          <View className='td'>1</View>
+          <View className='td'>2</View>
+          <View className='td'>3</View>
+        </View>
+      </View>
     </View>
   }
 }
